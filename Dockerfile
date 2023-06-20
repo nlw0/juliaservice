eea16470cb6e463b8f5835a6f2d8d72929bfd076
Nicolau Leal Werneck <nicolau@autofilltech.com>
2023-06-20 22:18:31 +0200

    this version seems to work

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM julia:1.9.0-bullseye
 
 ENV JULIA_CPU_TARGET=generic;broadwell;haswell;cascadelake;skylake;skylake-avx512;tigerlake
 
+# RUN apt-get update
+# RUN apt-get install -y gdb
+
 RUN useradd -ms /bin/bash app
 USER app
 
@@ -14,3 +17,5 @@ COPY . ./
 
 ENV JULIA_NUM_THREADS=auto
 CMD exec julia -e '@show Sys.CPU_NAME; using InteractiveUtils; versioninfo(); include("main-nohttp.jl")'
+
+# CMD exec gdb -batch -ex "run" -ex "bt" --args /usr/local/julia/bin/julia main-nohttp.jl
--- main-nohttp.jl
+++ main-nohttp.jl
@@ -1,5 +1,30 @@
 using Dates
 
+macro spawnprint(ex)
+    quote
+        Threads.@spawn try
+            $(esc(ex))
+        catch e
+            @error "Exception in task" exception=(e, catch_backtrace())
+        end
+    end
+end
+
+function mypmap(f, c...)
+    fx = map(c...) do x
+        @spawnprint f(x)
+    end
+    fetch.(fx)
+end
+
+function mypforeach(f, c...)
+    fx = map(c...) do x
+        @spawnprint f(x)
+    end
+    foreach(fetch, fx)
+end
+
+
 println("Pre-loop salute $(now())")
 for it in 1:11
     mydata = zeros(UInt64, 1024, 16)
@@ -9,7 +34,10 @@ for it in 1:11
     # end
     # println("Pre-thread salute $it $(minimum(mydata)) $(maximum(mydata)) $(now())")
 
-    Threads.@threads for n in 1:16
+    # Threads.@threads for n in 1:16
+    mypforeach(1:16) do n
+        # mydata[:,n+1] .= rand(UInt64,1024)
+        # @inbounds mydata[:,n+1] .= rand(UInt64,1024)
         mydata[:,n] .= rand(UInt64,1024)
     end
     println("Hello $it $(minimum(mydata)) $(maximum(mydata)) $(now())")
